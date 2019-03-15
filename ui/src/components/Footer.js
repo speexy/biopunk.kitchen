27e408f430f4461faa949788336223fa187816a4
speexy <jennifer.meyer@posteo.de>
2019-03-15 14:46:48 +0100

    add details to footer

--- ui/src/components/Footer.js
+++ ui/src/components/Footer.js
@@ -7,7 +7,13 @@ const Footer = () => (
     <div className={styles.footer}>
         <a href="https://www.openstreetmap.de/karte.html?zoom=15&lat=52.38885&lon=13.07688&layers=B000" target="_black" style={{ backgroundImage: `url(${Map})` }}></a>
         <div>
-            <Link to={'/legal'} > Imprint & Privacy </Link>
+            <p>Wissenschaftsladen Potsdam e.V.</p>
+            <p>Friedrich-Engels-Straße 22</p>
+            <p>Haus1 freiLand Potsdam (machBar @Haus5) </p>
+            <p>Germany - 14469 Potsdam </p>
+            <p>Banc Account: DE61 1203 0000 1020 5274 02</p>
+            <p>Contact: info@wissenschaftsladen-potsdam.de</p>
+            <Link to={'/legal'} > >> Imprint & Privacy </Link>
         </div>
     </div>
 );
